6688de787f75cf2e96d54e8f5fde97f66b97d66a
root <acalando@free.fr>
2017-09-22 18:47:32 +0200

    Title management and help

--- genpict.zsh
+++ genpict.zsh
@@ -10,13 +10,6 @@
 # version 0.1 - sept 2017
 #
 
-# Generic const and setup
-setopt extendedglob
-ESC_CHAR=$(printf '\033')
-ANSI_RED=$ESC_CHAR'[31m'
-ANSI_CYAN=$ESC_CHAR'[36m'
-ANSI_OFF=$ESC_CHAR'[0m'
-
 # #################################################################################################################
 # Genpict const
 
@@ -33,22 +26,25 @@ JPEG_QUALITY=80
 VIDEO_QUALITY=(30 slower)
 
 # Text and background colors
-typeset -A THEMES
-THEMES=( white "color:#000000; background-color:#FFFFFF;" \
+typeset -A COLORS
+COLORS=( white "color:#000000; background-color:#FFFFFF;" \
 	grey  "color:#CCCCCC; background-color:#222222;" \
 	black "color:#FFFFFF; background-color:#000000;" )
 
 # Simultaneous job count
 JOBS_MAX=$(nproc)
 
-# Parse options here to declare default arguments
-zparseopts -D -K t:=title s:=subtitle o:=outdir m:=theme d=dbg f=force h=help
+TOP_FORMAT=(40px 25px)
+LEFT_FORMAT=(70px 50px 60% auto)
 
-# Default arguments
-title=${title[2]:-"My Photos"}
-subtitle=${subtitle[2]:-"My not so short subtitle"}
-outdir=${outdir[2]:-html}
-theme=${theme[2]:-white}
+
+# #################################################################################################################
+# Generic const and setup
+setopt extendedglob
+ESC_CHAR=$(printf '\033')
+ANSI_RED=$ESC_CHAR'[31m'
+ANSI_CYAN=$ESC_CHAR'[36m'
+ANSI_OFF=$ESC_CHAR'[0m'
 
 # Generic functions
 error() {
@@ -70,6 +66,23 @@ para() {
 # #################################################################################################################
 # Genpict script
 
+# Parse options here to declare default arguments
+zparseopts -D -K t:=title T:=subtitle s:=side f:=format o:=outdir c:=color d=dbg r=remove h=help
+
+# Default arguments
+title=${title[2]-"My Photos"}
+subtitle=${subtitle[2]-"My not so short subtitle"}
+outdir=${outdir[2]-html}
+color=${color[2]-white}
+side=${side[2]-left}
+case $side in
+ (none) 	;;
+ (top) 		format=( ${=format[2]-$TOP_FORMAT} ) ;;
+ (left|right) 	format=( ${=format[2]-$LEFT_FORMAT} ) ;;
+ (*) 		error "Unkown title type: $side";;
+esac
+
+
 # help ?
 [[ -n $help ]] && {
 	print "Usage: genpict.zsh [options] [input folders]\n"	\
@@ -77,18 +90,33 @@ para() {
 		"Simple static pictures gallery generator.\n" \
 		"\n" \
 		"	-t title 	: title \n" \
-		"	-s subtitle	: subtitle\n" \
-		"	-m theme	: theme (white, grey, black)\n" \
+		"	-T subtitle	: subtitle \n" \
+		"	-s side		: title side (none,top,left,right)\n" \
+		"	-f \"format\"	: title format (\"size_t size_T pos_Y pos_X\")" \
+		"	-c color	: color (white, grey, black)\n" \
 		"	-o out		: output folder (def: html)\n" \
 		"	-d 		: debug (quick pass without copy and convertion)\n" \
-		"	-f		: remove existing output folder if any \n" \
-		"	"
+		"	-r		: remove existing output folder if any \n" \
+		"	\n" \
+		"Title and subtitles are on the left side by default, overlayed over the first image.\n" \
+		"other arguments are none (no title), top (centered over the image), right (overlayed on the right).\n" \
+		"\n" \
+		"Default format is \"$LEFT_FORMAT\": 1st and 2nd arguments are title and subtitle font size,\n" \
+		"3rd and 4th are used with left/right titles and are the vertical and horizontal position.\n" \
+		"These arguments use standard browser units: px, %, auto...\n" \
+		"\n" \
+		"Subtitle can be removed by setting it to \"\".\n" \
+		"\n" \
+
 	exit 1 }
 
+
+
 # Basic checks
-[[ -z $THEMES[$theme] ]] && error "Unkown theme \"$theme\""
+[[ -z $COLORS[$color] ]] && error "Unkown color \"$color\""
+[[ $side != none && $side != top && $side != left && $side != right ]] && error "Unkown title type \"$side\""
 (( $+commands[ffmpeg] && $+commands[ffprobe] )) || error "Cannot find ffmpeg/ffprobe"
-(( $+commands[convert] )) || error "Cannot find Image Magick/convert"
+(( $+commands[convert] && $+commands[identify] )) || error "Cannot find Image Magick convert/identify"
 (( $+commands[exiftool] )) || error "Cannot find exiftool"
 (( $+commands[jpegtran] )) || error "Cannot find jpegtran"	# ubuntu 16.04 pkg: libjpeg-progs
 
@@ -98,7 +126,9 @@ indir=( ${*:-.} )
 
 print "Title:    " $ANSI_CYAN$title	$ANSI_OFF
 print "Subtitle: " $ANSI_CYAN$subtitle	$ANSI_OFF
-print "Theme:    " $ANSI_CYAN$theme	$ANSI_OFF
+print "Side:     " $ANSI_CYAN$side	$ANSI_OFF
+print "Format:   " $ANSI_CYAN$format	$ANSI_OFF
+print "Color:    " $ANSI_CYAN$color	$ANSI_OFF
 print "In dir(s):" $ANSI_CYAN$indir	$ANSI_OFF
 print "Out dir:  " $ANSI_CYAN$outdir	$ANSI_OFF
 print
@@ -107,7 +137,8 @@ print
 indir_rec=( ${^indir}(/N) ${^indir}/**/*~**/$outdir/**~**/$outdir(/N) )
 [[ -z "$indir_rec" ]] && error "No directory found"
 
-# Get all the images and videos
+# Scan recursively images and videos and remember paths
+print "Scanning folders..."
 media=()
 for f in ${^indir_rec}/*(.) ; do
 	ext="$f:e:l"
@@ -116,28 +147,19 @@ done
 [[ -z $media ]] && error "No image or video found"
 
 # Create output dir
-[[ -n $force ]] && rm -fR $outdir
+[[ -n $remove ]] && rm -fR $outdir
 [[ -d $outdir ]] && error "Remove existing $outdir"
 mkdir $outdir
 
-# console.log('Space pressed ' + document.documentElement.scrollTop + ' ' + window.pageYOffset);
-
+print "Generating HTML..."
 # create HTML file with style and some JS to scroll to images with space/shift+space
+# console.log();
 cat << EOF > $outdir/index.html
 <!DOCTYPE html>
 <html>
 
 <head>
 	<meta charset="utf-8">
-
-	<style>
-	html	   { text-align: center;
-		     margin: auto;
-		     width: 1536px; }
-	img, video { margin:12px ; }
-	body	   { $THEMES[$theme] }
-	</style>
-
 	<script type="text/javascript">
 		addEventListener("keypress", function (ev) {
 			if (ev.keyCode === 0 || ev.keyCode === 32) {
@@ -168,14 +190,54 @@ cat << EOF > $outdir/index.html
 		});
 	</script>
 	<title>$title</title>
+	<style>
+	body	   { margin: auto;
+		     width: ${WIDTH_MAX}px;
+		     $COLORS[$color] }
+	img, video { margin: 12px auto;
+		     display: block;}
+	.footer { text-align: right; }
+EOF
+
+# Fill CSS depending on title type
+if [[ $side == top ]]; then
+	cat <<- EOF >> $outdir/index.html
+	 	.span1 { font-size: $format[1]; }
+	 	.span2 { font-size: $format[2]; }
+	 	.title { text-align: center; }
+	EOF
+
+elif [[ $side == left || $side == right ]]; then
+	cat <<- EOF >> $outdir/index.html
+	 	span { background: rgba(0,0,0,.7);
+	 		font: bold 50px/100px Arial;
+	 		padding:10px; }
+	 	.span1 { font-size: $format[1]; }
+	 	.span2 { font-size: $format[2]; }
+	 	.title { text-align: $side;
+	 		position: absolute;
+	 		top: $format[3];
+	 		$side: $format[4]; }
+	EOF
+elif [[ $side == none ]]; then
+	cat <<- EOF >> $outdir/index.html
+	 	.title { display: none; }
+	EOF
+fi
+
+# Line break and span only if subtitle exists
+[[ -n $subtitle ]] && span_subtitle="<br><span class=\"span2\">&thinsp;$subtitle</span><br>"
+
+# Close style/head and output title and subtitle
+cat << EOF >> $outdir/index.html
+	</style>
 </head>
-
-
 <body>
-
-<h1>$title</h1>
-<h2>$subtitle</h2>
-
+	<div>
+		<p class="title">
+			<span class="span1">&thinsp;$title</span>
+			$span_subtitle
+		</p>
 EOF
 
 # main loop to process all images
@@ -184,7 +246,7 @@ for f in $media ; do
 	ext="$f:e:l"
 	if (( $+IMG_EXT[(r)$ext] )); then
 
-		print "Processing image $f..."
+		print "  Processing image $f"
 		dest="$f:t"
 		dest_sm="${dest:r}_sm.jpg"
 
@@ -220,7 +282,7 @@ for f in $media ; do
 			# add HTML tags
 			cat <<- EOF >> $outdir/index.html
 
-			<a href="$dest"><img src="$dest_sm" alt="$dest" id="media$cnt"></a><br>
+			 	<a href="$dest"><img src="$dest_sm" alt="$dest" id="media$cnt"></a><br>
 			EOF
 
 		else
@@ -230,13 +292,13 @@ for f in $media ; do
 			# add HTML tags
 			cat <<- EOF >> $outdir/index.html
 
-			<a href="$f:t"><img src="$f:t" alt="$f:t" width="$width_n" height="$height_n" id="media$cnt"></a><br>
+			 	<a href="$f:t"><img src="$f:t" alt="$f:t" width="$width_n" height="$height_n" id="media$cnt"></a><br>
 			EOF
 
 		fi
 	elif (( $+VID_EXT[(r)$ext] )); then
 
-		print "Processing video $f..."
+		print "  Processing video $f"
 		dest="${f:t:r}.mp4"
 		dest_sm="${dest:r}_sm.jpg"
 
@@ -279,9 +341,9 @@ for f in $media ; do
 
 			cat <<- EOF >> $outdir/index.html
 
-			<video poster="$dest_sm" onclick="this.paused?this.play():this.pause();" id="media$cnt">
-				<source src="$dest" type="video/mp4" />
-			</video><br>
+			 	<video poster="$dest_sm" onclick="this.paused?this.play():this.pause();" id="media$cnt">
+			 		<source src="$dest" type="video/mp4" />
+			 	</video><br>
 			EOF
 		else
 			# dbg: only file links and no resizing
@@ -290,26 +352,31 @@ for f in $media ; do
 			# add HTML tags
 			cat <<- EOF >> $outdir/index.html
 
-			<video width="$width_n" height="$height_n" onclick="this.paused?this.play():this.pause();" id="media$cnt">
-				<source src="$dest" type="video/mp4" />
-			</video><br>
-
+			 	<video width="$width_n" height="$height_n" onclick="this.paused?this.play():this.pause();" id="media$cnt">
+			 		<source src="$dest" type="video/mp4" />
+			 	</video><br>
 			EOF
 		fi
 	fi
-	((cnt++))
+	# Close div overlay after the first image only
+	((cnt++ == 0 )) && cat <<- EOF >> $outdir/index.html
+		 	</div>
+		EOF
+
 done
 
-# add footer
+# add footer. id is added on purpose to scroll up to this footer
 cat << EOF >> $outdir/index.html
 
-<p align='right' id='media$cnt'> Generated $(date) by genpict </p>
+	<p class='footer' id='media$cnt'> Generated $(date) by genpict<br>Space / Shift+space to scroll<br>Click on pictures for full size file</p>
 
 </body>
 </html>
 EOF
 
+print "Done, waiting for last jobs..."
+
 # wait for all jobs to finish
 wait
-rm -f "$outdir/*.tmp"
+rm -f $outdir/*.tmp
 
